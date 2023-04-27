f0ca930475f9b0f1ae83189f3519c71e9d2d6a7f
Nhat Tien <ledangnhattien04@gmail.com>
2023-04-27 09:52:29 +0700

    fix UI

--- containers/HomeContainer/index.js
+++ containers/HomeContainer/index.js
@@ -2,6 +2,14 @@ import Image from "next/image";
 import styles from "./styles.module.scss"
 import bg from "../../public/imgs/backgound.png"
 import Button from "~/components/base/Button";
+import ImageSlider from "~/components/common/ImageSlider";
+
+const slides = [
+  {url:'https://images.unsplash.com/photo-1681589436610-ee3b350e57d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'},
+  {url:'https://images.unsplash.com/photo-1681651987346-a6db3ba3441f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80'},
+  {url:'https://images.unsplash.com/photo-1682106446688-19a29ffb0b91?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'},
+]
+
 
 function HomeContainer() {
   return (
@@ -22,8 +30,12 @@ function HomeContainer() {
     </section>
     <section className={styles['explore']}>
       <div className={styles['explore__img']}>
-      <Image src={'/imgs/exploring.svg'} width={100} height={100}/>
+        <div className="flex flex-col">
+          <Image src={'/imgs/exploring.svg'} width={80} height={80}/>
+          <p>KHÁM PHÁ</p>
+        </div>
       </div>
+        {/*<ImageSlider slides={slides} />*/}
     </section>
   </div>
   )
