0161d45c49a409166213e7df3f8bd6b842f2151b
Nhat Tien <ledangnhattien04@gmail.com>
2023-05-14 20:01:51 +0700

    add countContinuousDay

--- utils/countContinuousDay.js
+++ utils/countContinuousDay.js
@@ -1,11 +1,16 @@
+import getCurrentDate from "./getCurrentDate";
 import isAfterDate from "./isAfterDate";
 
 export default function countContinuousDay(dateArray) {
     var count = 1;
     var index = dateArray.length-1;
-    while(isAfterDate(dateArray[index-1],dateArray[index])) {
-        count++;
-        index--;
+    if(isAfterDate(dateArray[index],getCurrentDate()) || dateArray[index] === getCurrentDate()) {
+        while(isAfterDate(dateArray[index-1],dateArray[index])) {
+            count++;
+            index--;
+        }
+    } else {
+        count = 0
     }
     return count;
 } 
