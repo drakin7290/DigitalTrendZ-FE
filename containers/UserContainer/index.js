74891c9894bf86dfa46de13628d46f747972ccc7
Nhat Tien <ledangnhattien04@gmail.com>
2023-06-05 15:27:22 +0700

    change userpage illustration

--- containers/UserContainer/index.js
+++ containers/UserContainer/index.js
@@ -1,9 +1,10 @@
 import getUser from "~/utils/getUser"
 import styles from "./styles.module.scss"
+import Image from "next/image";
 
 export default function UserContainer() {
-const userName = getUser().name;
 
+const userName = getUser().name ?? "";
 const getFirstName = (name) => {
     const nameArray = name.split(" ");
     return `${nameArray[0]} ${nameArray[nameArray.length-1]}`
@@ -12,8 +13,9 @@ const firstName = getFirstName(userName);
 
 return (
     <main className={styles['main']}>
-        <div className={styles['heading']}>Chào bạn {firstName}</div>
-        <div className={styles['computer-container']}>
+        <div className={styles['heading']}>Chào bạn <span className={styles['heading__username']}>{firstName}</span></div>
+        <div className={styles['illus-container']}>
+            <Image src="/imgs/user_illus.svg" width={400} height={400} priority alt="user_illus" />
         </div>
     </main>
 )
--- pages/index.js
+++ pages/index.js
@@ -15,8 +15,8 @@ export default function HomePage() {
   return (
     <MainLayout>
         {logged 
-         ? <UserContainer />
-         : <HomeContainer />
+        ? <UserContainer />
+        : <HomeContainer /> 
         }
     </MainLayout>
   );
